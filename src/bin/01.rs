26393f6b0c74ea092743279026da162eaeaf1eba
Guillaume Dupuis <guillaumedupuis@Guillaumes-Laptop.local>
2024-12-01 13:49:51 -0500

    2nd gold star

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,8 +1,42 @@
-use std::path::absolute;
+use std::{collections::HashMap, iter::Map, path::absolute};
 
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let (mut left, mut right) = deser(input)?;
+
+    left.sort();
+    right.sort();
+
+    let mut sum = 0;
+    for i in 0..left.len() {
+        sum += left[i].abs_diff(right[i]);
+    }
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (left, right) = deser(input)?;
+
+    let mut dict = HashMap::<u32, u32>::new();
+
+    for r in right {
+        let count = dict.entry(r).or_insert(0);
+        *count += 1;
+    }
+
+    let mut similarity = 0;
+    for l in left {
+        if let Some(i) = dict.get(&l) {
+            similarity += i * l;
+        }
+    }
+
+    Some(similarity)
+}
+
+fn deser(input: &str) -> Option<(Vec<u32>, Vec<u32>)> {
     let mut left = Vec::new();
     let mut right = Vec::new();
     for line in input.lines() {
@@ -18,18 +52,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     }
 
-    left.sort();
-    right.sort();
-    let mut sum = 0;
-    for i in 0..left.len() {
-        sum += left[i].abs_diff(right[i]);
-    }
-
-    Some(sum)
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some((left, right))
 }
 
 #[cfg(test)]
@@ -45,6 +68,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
