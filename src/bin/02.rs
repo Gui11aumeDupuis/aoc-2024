97852abbe21b17d40ac104fa642f4f147e5ff28e
Guillaume Dupuis <guillaumedupuis@Guillaumes-Laptop.local>
2024-12-02 22:26:03 -0500

    extract check_report function

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -4,24 +4,32 @@ pub fn part_one(input: &str) -> Option<u32> {
     let reports = deser(input);
     let mut safe_count: u32 = 0;
     for r in reports {
-        let mut n = r[0];
-        let asc = r[1] > r[0];
-        safe_count += 1;
-        for m in r.iter().skip(1) {
-            let diff = m - n;
-            if (diff == 0) || (asc && diff < 0) || (!asc && diff > 0) || (diff.abs_diff(0) > 3) {
-                safe_count -= 1;
-                break;
-            }
-
-            n = *m
+        if check_report(r) {
+            safe_count += 1;
         }
     }
     Some(safe_count)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let reports = deser(input);
+    let mut safe_count: u32 = 0;
+
+    Some(safe_count)
+}
+
+fn check_report(r: Vec<i32>) -> bool {
+    let mut n = r[0];
+    let asc = r[1] > r[0];
+    for m in r.iter().skip(1) {
+        let diff = m - n;
+        if (diff == 0) || (asc && diff < 0) || (!asc && diff > 0) || (diff.abs_diff(0) > 3) {
+            return false;
+        }
+
+        n = *m
+    }
+    true
 }
 
 fn deser(input: &str) -> Vec<Vec<i32>> {
@@ -46,6 +54,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
