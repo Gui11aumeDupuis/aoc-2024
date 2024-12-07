f4af980ff3ed293a1555dba323a3dc3603698389
Guillaume Dupuis <guillaumedupuis@Guillaumes-Laptop.local>
2024-12-07 12:39:52 -0500

    some refactoring for fun

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,29 +1,29 @@
+use std::ops::Add;
+
 advent_of_code::solution!(6);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut grid = deser(input);
-    let mut guard_pos = find_gard(&grid)?;
-    grid[guard_pos.1][guard_pos.0] = 'X';
-    let directions = [(0, -1), (1, 0), (0, 1), (-1, 0)];
-    let mut guard_dir: usize = 0;
+    let mut chars = deser(input);
+
+    let mut lab = Lab { grid: &mut chars };
+    let mut guard = lab.find_gard()?;
+
+    lab.modify(&guard.pos, 'X');
     let mut visited: u32 = 1;
 
-    while guard_in_bound(guard_pos.0, guard_pos.1, directions[guard_dir], &grid) {
-        let new_guard_pos = (
-            (guard_pos.0 as i32 + directions[guard_dir].0) as usize,
-            (guard_pos.1 as i32 + directions[guard_dir].1) as usize,
-        );
+    while guard.in_bound(&lab) {
+        let next_pos = guard.get_next_pos();
 
-        let step_val = grid[new_guard_pos.1][new_guard_pos.0];
+        let step_val = lab.get(&next_pos);
 
         if step_val == '#' {
-            guard_dir = (guard_dir + 1) % 4;
+            guard.change_direction();
         } else if step_val != 'X' {
             visited += 1;
-            grid[new_guard_pos.1][new_guard_pos.0] = 'X';
-            guard_pos = (new_guard_pos.0, new_guard_pos.1)
+            lab.modify(&next_pos, 'X');
+            guard.make_step();
         } else {
-            guard_pos = (new_guard_pos.0, new_guard_pos.1)
+            guard.make_step();
         }
     }
 
@@ -47,29 +47,114 @@ fn deser(input: &str) -> Vec<Vec<char>> {
     grid
 }
 
-fn find_gard(grid: &[Vec<char>]) -> Option<(usize, usize)> {
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            if grid[y][x] == '^' {
-                return Some((x, y));
-            }
-        }
-    }
-    None
+struct Lab<'a> {
+    grid: &'a mut [Vec<char>],
 }
 
-fn guard_in_bound(x: usize, y: usize, direction: (i32, i32), grid: &[Vec<char>]) -> bool {
-    let guard_step_x = x as i32 + direction.0;
-    let guard_step_y = y as i32 + direction.1;
-    if (guard_step_x) < 0
-        || (guard_step_x) >= grid[0].len() as i32
-        || (guard_step_y) < 0
-        || (guard_step_y) >= grid.len() as i32
-    {
-        return false;
+impl<'a> Lab<'a> {
+    fn width(&self) -> usize {
+        self.grid[0].len()
     }
 
-    true
+    fn height(&self) -> usize {
+        self.grid.len()
+    }
+
+    fn find_gard(&self) -> Option<Guard> {
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                if self.grid[y][x] == '^' {
+                    return Some(Guard {
+                        pos: Pos {
+                            x: x as i32,
+                            y: y as i32,
+                        },
+                        dir: Direction::Up,
+                    });
+                }
+            }
+        }
+        None
+    }
+
+    fn modify(&mut self, pos: &Pos, val: char) {
+        self.grid[pos.y as usize][pos.x as usize] = val;
+    }
+
+    fn get(&self, pos: &Pos) -> char {
+        self.grid[pos.y as usize][pos.x as usize]
+    }
+}
+
+struct Guard {
+    pos: Pos,
+    dir: Direction,
+}
+
+impl Guard {
+    fn change_direction(&mut self) {
+        match self.dir {
+            Direction::Left => self.dir = Direction::Up,
+            Direction::Right => self.dir = Direction::Down,
+            Direction::Up => self.dir = Direction::Right,
+            Direction::Down => self.dir = Direction::Left,
+        }
+    }
+
+    fn in_bound(&self, lab: &Lab) -> bool {
+        let next = self.get_next_pos();
+        if (next.x) < 0
+            || (next.x) >= lab.width() as i32
+            || (next.y) < 0
+            || (next.y) >= lab.height() as i32
+        {
+            return false;
+        }
+
+        true
+    }
+
+    fn make_step(&mut self) {
+        self.pos = self.get_next_pos();
+    }
+
+    fn get_next_pos(&self) -> Pos {
+        self.pos + self.dir.step()
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Pos {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+impl Direction {
+    fn step(&self) -> Pos {
+        match *self {
+            Direction::Left => Pos { x: -1, y: 0 },
+            Direction::Right => Pos { x: 1, y: 0 },
+            Direction::Up => Pos { x: 0, y: -1 },
+            Direction::Down => Pos { x: 0, y: 1 },
+        }
+    }
 }
 
 #[cfg(test)]
